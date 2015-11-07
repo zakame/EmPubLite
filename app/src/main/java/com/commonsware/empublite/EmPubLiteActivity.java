08cd895cb0e55103c1147a5665f1f67a35b2baae
Zak B. Elep <zakame@zakame.net>
2015-11-07 22:58:47 +0800

    Add support for adding and removing chapter notes

--- app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -57,6 +57,11 @@ public class EmPubLiteActivity extends Activity {
         case R.id.settings:
             startActivity(new Intent(this, Preferences.class));
             return true;
+        case R.id.notes:
+            i = new Intent(this, NoteActivity.class);
+            i.putExtra(NoteActivity.EXTRA_POSITION, pager.getCurrentItem());
+            startActivity(i);
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
