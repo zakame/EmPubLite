9cffd84f00f73c276094a1af3e44c32b71f2e5dd
Zak B. Elep <zakame@zakame.net>
2015-11-09 04:47:26 +0800

    add simple support for large screens

--- app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -1,25 +1,36 @@
 package com.commonsware.empublite;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v4.view.ViewPager;
 import de.greenrobot.event.EventBus;
 
-public class EmPubLiteActivity extends Activity {
+public class EmPubLiteActivity extends Activity implements FragmentManager.OnBackStackChangedListener {
+    private View sidebar = null;
+    private View divider = null;
     private ViewPager pager = null;
     private ContentsAdapter adapter = null;
     private ModelFragment mfrag = null;
+    private SimpleContentFragment help = null;
+    private SimpleContentFragment about = null;
 
     private static final String MODEL = "model";
     private static final String PREF_LAST_POSITION = "lastPosition";
     private static final String PREF_SAVE_LAST_POSITION = "saveLastPosition";
     private static final String PREF_KEEP_SCREEN_ON = "keepScreenOn";
+    private static final String HELP = "help";
+    private static final String ABOUT = "about";
+
+    private static final String FILE_HELP = "file:///android_asset/misc/help.html";
+    private static final String FILE_ABOUT = "file:///android_asset/misc/about.html";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,10 @@ public class EmPubLiteActivity extends Activity {
 
         setContentView(R.layout.main);
         pager = (ViewPager) findViewById(R.id.pager);
+        sidebar = findViewById(R.id.sidebar);
+        divider = findViewById(R.id.divider);
+        help = (SimpleContentFragment) getFragmentManager().findFragmentByTag(HELP);
+        about = (SimpleContentFragment) getFragmentManager().findFragmentByTag(ABOUT);
     }
 
     @Override
@@ -41,24 +56,16 @@ public class EmPubLiteActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
         case R.id.about:
-            Intent i = new Intent(this, SimpleContentActivity.class)
-                .putExtra(SimpleContentActivity.EXTRA_FILE,
-                          "file:///android_asset/misc/about.html");
-            startActivity(i);
-
+            showAbout();
             return true;
         case R.id.help:
-            i = new Intent(this, SimpleContentActivity.class)
-                .putExtra(SimpleContentActivity.EXTRA_FILE,
-                          "file:///android_asset/misc/help.html");
-            startActivity(i);
-
+            showHelp();
             return true;
         case R.id.settings:
             startActivity(new Intent(this, Preferences.class));
             return true;
         case R.id.notes:
-            i = new Intent(this, NoteActivity.class);
+            Intent i = new Intent(this, NoteActivity.class);
             i.putExtra(NoteActivity.EXTRA_POSITION, pager.getCurrentItem());
             startActivity(i);
             return true;
@@ -101,6 +108,18 @@ public class EmPubLiteActivity extends Activity {
         super.onPause();
     }
 
+    @Override
+    public void onBackStackChanged() {
+        if (getFragmentManager().getBackStackEntryCount() == 0) {
+            LinearLayout.LayoutParams p = (LinearLayout.LayoutParams) sidebar.getLayoutParams();
+            if (p.weight > 0) {
+                p.weight = 0;
+                sidebar.setLayoutParams(p);
+                divider.setVisibility(View.GONE);
+            }
+        }
+    }
+
     @SuppressWarnings("unused")
     public void onEventMainThread(BookLoadedEvent event) {
         setupPager(event.getBook());
@@ -131,5 +150,54 @@ public class EmPubLiteActivity extends Activity {
         }
 
         StrictMode.setThreadPolicy(builder.build());
+    }
+
+
+    private void showAbout() {
+        if (sidebar != null) {
+            openSidebar();
+
+            if (about == null) {
+                about = SimpleContentFragment.newInstance(FILE_ABOUT);
+            }
+
+            getFragmentManager().beginTransaction().addToBackStack(null)
+                .replace(R.id.sidebar, about, ABOUT).commit();
+        }
+        else {
+            Intent i = new Intent(this, SimpleContentActivity.class);
+
+            i.putExtra(SimpleContentActivity.EXTRA_FILE, FILE_ABOUT);
+            startActivity(i);
+        }
+    }
+
+    private void showHelp() {
+        if (sidebar != null) {
+            openSidebar();
+
+            if (help == null) {
+                help = SimpleContentFragment.newInstance(FILE_HELP);
+            }
+
+            getFragmentManager().beginTransaction().addToBackStack(null)
+                .replace(R.id.sidebar, help, HELP).commit();
+        }
+        else {
+            Intent i = new Intent(this, SimpleContentActivity.class);
+
+            i.putExtra(SimpleContentActivity.EXTRA_FILE, FILE_HELP);
+            startActivity(i);
+        }
+    }
+
+    private void openSidebar() {
+        LinearLayout.LayoutParams p = (LinearLayout.LayoutParams) sidebar.getLayoutParams();
+        if (p.weight == 0) {
+            p.weight = 3;
+            sidebar.setLayoutParams(p);
+        }
+
+        divider.setVisibility(View.VISIBLE);
     }
 }
